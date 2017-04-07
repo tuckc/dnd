0dc519e466ac5d410392b9c86cddda8cbeef87a5
tuckc <coltontuck94@gmail.com>
2017-04-07 16:24:52 -0400

    Add set_spells, form_fields methods
    set_spells sets all of the character's cantrips and spells
    get_form_fields returns a list of tuples to feed into fdfgen

--- character.py
+++ character.py
@@ -16,6 +16,7 @@ class Character(object):
         self._set_features()
         self._set_attacks()
         self.initiative = self.stats.dex_mod
+        self._set_spells()
 
     def _set_header(self, player_name):
         self.character_name = self.background.name
@@ -138,26 +139,26 @@ class Character(object):
             martial_melee = json.load(martial_m)
             martial_ranged = json.load(martial_r)
 
-        attacks = [{}, {}, {}]
+        self.attacks = [{}, {}, {}]
         count = 0
         for equipment in self.character_class.equipment:
             if count > 2:
                 break
             if equipment in [wep['Name'] for wep in simple_melee]:
                 weapon = self._get_weapon(equipment, simple_melee)
-                self._set_melee_stats(attacks, equipment, 'Simple Weapons', weapon, count)
+                self._set_melee_stats(self.attacks, equipment, 'Simple Weapons', weapon, count)
                 count += 1
             elif equipment in [wep['Name'] for wep in simple_ranged]:
                 weapon = self._get_weapon(equipment, simple_ranged)
-                self._set_ranged_stats(attacks, equipment, 'Simple Weapons', weapon, count)
+                self._set_ranged_stats(self.attacks, equipment, 'Simple Weapons', weapon, count)
                 count += 1
             elif equipment in [wep['Name'] for wep in martial_melee]:
                 weapon = self._get_weapon(equipment, martial_melee)
-                self._set_melee_stats(attacks, equipment, 'Martial Weapons', weapon, count)
+                self._set_melee_stats(self.attacks, equipment, 'Martial Weapons', weapon, count)
                 count += 1
             elif equipment in [wep['Name'] for wep in martial_ranged]:
                 weapon = self._get_weapon(equipment, martial_ranged)
-                self._set_ranged_stats(attacks, equipment, 'Martial Weapons', weapon, count)
+                self._set_ranged_stats(self.attacks, equipment, 'Martial Weapons', weapon, count)
                 count += 1
 
     def _get_weapon(self, name, weapons):
@@ -169,19 +170,141 @@ class Character(object):
     def _set_melee_stats(self, info, equipment, weapon_type, weapon, count):
         if equipment+'s' in self.other or weapon_type in self.other:
             info[count]['Name'] = weapon['Name']
-            info[count]['AtkBonus'] = str(self.stats.str_mod + 2)
-            info[count]['Damage'] = weapon['Damage'] + '+' + str(self.stats.str_mod)
+            info[count]['AtkBonus'] = '+' + str(self.stats.str_mod + 2) if self.stats.str_mod + 2 > 0 else self.stats.str_mod
+            if self.stats.str_mod >= 0:
+                info[count]['Damage'] = weapon['Damage'] + '+' + str(self.stats.str_mod)
+            else:
+                info[count]['Damage'] = weapon['Damage'] + str(self.stats.str_mod)
         else:
             info[count]['Name'] = weapon['Name']
-            info[count]['AtkBonus'] = str(self.stats.str_mod)
-            info[count]['Damage'] = weapon['Damage'] + '+' + str(self.stats.str_mod)
+            info[count]['AtkBonus'] = '+' + str(self.stats.str_mod) if self.stats.str_mod > 0 else self.stats.str_mod
+            if self.stats.str_mod >= 0:
+                info[count]['Damage'] = weapon['Damage'] + '+' + str(self.stats.str_mod)
+            else:
+                info[count]['Damage'] = weapon['Damage'] + str(self.stats.str_mod)
 
     def _set_ranged_stats(self, info, equipment, weapon_type, weapon, count):
         if equipment+'s' in self.other or weapon_type in self.other:
             info[count]['Name'] = weapon['Name']
-            info[count]['AtkBonus'] = str(self.stats.dex_mod + 2)
-            info[count]['Damage'] = weapon['Damage'] + '+' + str(self.stats.dex_mod)
+            info[count]['AtkBonus'] = '+' + str(self.stats.dex_mod + 2) if self.stats.dex_mod + 2 > 0 else self.stats.dex_mod
+            if self.stats.dex_mod >= 0:
+                info[count]['Damage'] = weapon['Damage'] + '+' + str(self.stats.dex_mod)
+            else:
+                info[count]['Damage'] = weapon['Damage'] + str(self.stats.dex_mod)
         else:
             info[count]['Name'] = weapon['Name']
-            info[count]['AtkBonus'] = str(self.stats.dex_mod)
-            info[count]['Damage'] = weapon['Damage'] + '+' + str(self.stats.dex_mod)
+            info[count]['AtkBonus'] = '+' + str(self.stats.dex_mod) if self.stats.dex_mod > 0 else self.stats.dex_mod
+            if self.stats.dex_mod >= 0:
+                info[count]['Damage'] = weapon['Damage'] + '+' + str(self.stats.dex_mod)
+            else:
+                info[count]['Damage'] = weapon['Damage'] + str(self.stats.dex_mod)
+
+    def _set_spells(self):
+        self.spells = []
+        self.spells.extend(self.race.cantrips)
+        self.spells.extend(self.character_class.cantrips)
+        self.spells.extend(self.character_class.spells)
+
+    def _set_lang_and_other(self):
+        self.other
+
+    def get_form_fields(self):
+        all_fields = []
+        all_fields.append(('ClassLevel', self.class_name))
+        all_fields.append(('Background', self.background_name))
+        all_fields.append(('PlayerName', self.player_name))
+        all_fields.append(('CharacterName', self.character_name))
+        all_fields.append(('Race ', self.race_name))
+        all_fields.append(('Alignment', self.alignment))
+        all_fields.append(('XP', self.experience))
+        all_fields.append(('STR', self.stats.strength))
+        all_fields.append(('ProfBonus', '+2'))
+        all_fields.append(('AC', self.armor_class))
+        all_fields.append(('Initiative', '+' + str(self.initiative) if self.initiative > 0 else str(self.initiative)))
+        all_fields.append(('Speed', self.speed))
+        all_fields.append(('PersonalityTraits ', self.background.personality))
+        all_fields.append(('STRmod', '+' + str(self.stats.str_mod) if self.stats.str_mod > 0 else self.stats.str_mod))
+        all_fields.append(('HPMax', self.hit_points))
+        all_fields.append(('ST Strength', self.save_mods['Strength']))
+        all_fields.append(('DEX', self.stats.dexterity))
+        all_fields.append(('HPCurrent', self.hit_points))
+        all_fields.append(('Ideals', self.background.ideals))
+        all_fields.append(('DEXmod ', '+' + str(self.stats.dex_mod) if self.stats.dex_mod > 0 else self.stats.dex_mod))
+        all_fields.append(('Bonds', self.background.bonds))
+        all_fields.append(('CON', self.stats.constitution))
+        all_fields.append(('HDTotal', '1'))
+        all_fields.append(('CONmod', '+' + str(self.stats.con_mod) if self.stats.con_mod > 0 else self.stats.con_mod))
+        all_fields.append(('HD', '1d' + str(self.character_class.hit_die)))
+        all_fields.append(('Flaws', self.background.flaws))
+        all_fields.append(('INT', self.stats.intelligence))
+        all_fields.append(('ST Dexterity', self.save_mods['Dexterity']))
+        all_fields.append(('ST Constitution', self.save_mods['Constitution']))
+        all_fields.append(('ST Intelligence', self.save_mods['Intelligence']))
+        all_fields.append(('ST Wisdom', self.save_mods['Wisdom']))
+        all_fields.append(('ST Charisma', self.save_mods['Charisma']))
+        all_fields.append(('Acrobatics', self.mods['Acrobatics']))
+        all_fields.append(('Animal', self.mods['Animal Handling']))
+        all_fields.append(('Athletics', self.mods['Athletics']))
+        all_fields.append(('Deception ', self.mods['Deception']))
+        all_fields.append(('History ', self.mods['History']))
+        all_fields.append(('Insight', self.mods['Insight']))
+        all_fields.append(('Intimidation', self.mods['Intimidation']))
+        all_fields.append(('Check Box 11', True if 'Strength' in self.character_class.saving_throws else False))
+        all_fields.append(('Check Box 18', True if 'Dexterity' in self.character_class.saving_throws else False))
+        all_fields.append(('Check Box 19', True if 'Constitution' in self.character_class.saving_throws else False))
+        all_fields.append(('Check Box 20', True if 'Intelligence' in self.character_class.saving_throws else False))
+        all_fields.append(('Check Box 21', True if 'Wisdom' in self.character_class.saving_throws else False))
+        all_fields.append(('Check Box 22', True if 'Charisma' in self.character_class.saving_throws else False))
+        all_fields.append(('Wpn Name', self.attacks[0]['Name'] if self.attacks[0] else ''))
+        all_fields.append(('Wpn1 AtkBonus', self.attacks[0]['AtkBonus'] if self.attacks[0] else ''))
+        all_fields.append(('Wpn1 Damage', self.attacks[0]['Damage'] if self.attacks[0] else ''))
+        all_fields.append(('INTmod', '+' + str(self.stats.int_mod) if self.stats.int_mod > 0 else self.stats.int_mod))
+        all_fields.append(('Wpn Name 2', self.attacks[1]['Name'] if self.attacks[1] else ''))
+        all_fields.append(('Wpn2 AtkBonus ', self.attacks[1]['AtkBonus'] if self.attacks[1] else ''))
+        all_fields.append(('Wpn2 Damage ', self.attacks[1]['Damage'] if self.attacks[1] else ''))
+        all_fields.append(('Investigation ', self.mods['Investigation']))
+        all_fields.append(('WIS', self.stats.wisdom))
+        all_fields.append(('Wpn Name 3', self.attacks[2]['Name'] if self.attacks[2] else ''))
+        all_fields.append(('Wpn3 AtkBonus', self.attacks[2]['AtkBonus'] if self.attacks[2] else ''))
+        all_fields.append(('Wpn3 Damage', self.attacks[2]['Damage'] if self.attacks[2] else ''))
+        all_fields.append(('Arcana', self.mods['Arcana']))
+        all_fields.append(('Perception ', self.mods['Perception']))
+        all_fields.append(('WISmod', '+' + str(self.stats.wis_mod) if self.stats.wis_mod > 0 else self.stats.wis_mod))
+        all_fields.append(('CHA', self.stats.charisma))
+        all_fields.append(('Nature', self.mods['Nature']))
+        all_fields.append(('Performance', self.mods['Performance']))
+        all_fields.append(('Medicine', self.mods['Medicine']))
+        all_fields.append(('Religion', self.mods['Religion']))
+        all_fields.append(('Stealth ', self.mods['Stealth']))
+        all_fields.append(('Check Box 23', True if 'Acrobatics' in self.proficiencies else False))
+        all_fields.append(('Check Box 24', True if 'Animal Handling' in self.proficiencies else False))
+        all_fields.append(('Check Box 25', True if 'Arcana' in self.proficiencies else False))
+        all_fields.append(('Check Box 26', True if 'Athletics' in self.proficiencies else False))
+        all_fields.append(('Check Box 27', True if 'Deception' in self.proficiencies else False))
+        all_fields.append(('Check Box 28', True if 'History' in self.proficiencies else False))
+        all_fields.append(('Check Box 29', True if 'Insight' in self.proficiencies else False))
+        all_fields.append(('Check Box 30', True if 'Intimidation' in self.proficiencies else False))
+        all_fields.append(('Check Box 31', True if 'Investigation' in self.proficiencies else False))
+        all_fields.append(('Check Box 32', True if 'Medicine' in self.proficiencies else False))
+        all_fields.append(('Check Box 33', True if 'Nature' in self.proficiencies else False))
+        all_fields.append(('Check Box 34', True if 'Perception' in self.proficiencies else False))
+        all_fields.append(('Check Box 35', True if 'Performance' in self.proficiencies else False))
+        all_fields.append(('Check Box 36', True if 'Persuasion' in self.proficiencies else False))
+        all_fields.append(('Check Box 37', True if 'Religion' in self.proficiencies else False))
+        all_fields.append(('Check Box 38', True if 'Sleight of Hand' in self.proficiencies else False))
+        all_fields.append(('Check Box 39', True if 'Stealth' in self.proficiencies else False))
+        all_fields.append(('Check Box 40', True if 'Survival' in self.proficiencies else False))
+        all_fields.append(('Persuasion', self.mods['Persuasion']))
+        all_fields.append(('SleightofHand', self.mods['Stealth']))
+        all_fields.append(('CHamod', '+' + str(self.stats.chr_mod) if self.stats.chr_mod > 0 else self.stats.chr_mod))
+        all_fields.append(('Survival', self.mods['Survival']))
+        all_fields.append(('AttacksSpellcasting', '\n'.join(self.spells)))
+        all_fields.append(('Passive', self.passive_perception))
+        languages = ', '.join(self.languages)
+        other = ', '.join(self.other)
+        both = languages + '\n\n' + other
+        all_fields.append(('ProficienciesLang', languages + '\n\n' + other))
+        all_fields.append(('Equipment', '\n'.join(self.character_class.equipment)))
+        all_fields.append(('Features and Traits', '\n'.join(self.features)))
+
+        return all_fields
