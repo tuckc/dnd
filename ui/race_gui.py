9d23fb097b2c69909d5c33a38940952532abb2c1
sergioYep <sy13@my.fsu.edu>
2017-04-08 20:39:06 -0700

    Add files via upload
    Need to make grid layout and pic a bit bigger.

---
+++ ui/race_gui.py
@@ -0,0 +1,80 @@
+#UserInterface.py
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+import sys
+
+class DND_CC_Window(QtWidgets.QWidget):
+	def __init__(self):
+		QtWidgets.QWidget.__init__(self)
+		self.setup()
+
+	def setup(self):
+		self.setGeometry(200, 200, 700, 700)
+		self.setWindowTitle("Dungeons and Dragons Character Creator")
+		
+		self.gnomeButton=QPushButton("Gnome")
+		self.gnomeButton.setToolTip("Gnomes take delight in life, enjoying every moment of invention, exploration, investigation, creation, and play.<br>Gnomes average slightly over 3 feet tall and w eigh 40 to 45 pounds.")
+		
+		
+		self.halflingButton=QPushButton("Halfling")
+		self.halflingButton.setToolTip("these wanderers love peace, food, hearth, and home, though home might be a wagon jostling along an dirt road or a raft floating downriver.<br>The diminutive halflings survive in a world full of larger creatures by avoiding notice or, barring that, avoiding offense.<br> Standing about 3 feet tall, they appear relatively harmless and so have managed to survive for centuries in the shadow of empires and on the edges of wars and political strife")
+		
+		self.dwarfButton=QPushButton("Dwarf")
+		self.dwarfButton.setToolTip("Bold and hardy, dwarves are known as skilled warriors, miners, and workers of stone and metal.<br> Though they stand well under 5 feet tall, dwarves are so broad and compact that they can weigh as much as a human standing nearly two feet taller.<br> Their courage and endurance are also easily a match for any of the larger folk.")
+
+		self.elfButton=QPushButton("Elf")
+		self.elfButton.setToolTip("Elves love nature and magic, art and artistry, music and poetry, and the good things of the world.<br>With their unearthly grace and fine features, elves appear hauntingly beautiful to humans and members of many other races.")
+
+		self.halfelfButton=QPushButton("Half-Elf")
+		self.halfelfButton.setToolTip("Walking in two w orlds but truly belonging to neither,half-elves combine what some say are the best qualities of their elf and human parents: human curiosity, inventiveness, and ambition tempered by the refined senses, love of nature, and artistic tastes o f the elves.<br>Many half-elves,unable to fit into either society, choose lives of solitary wandering or join with other misfits and outcasts in the adventuring life.")
+
+		self.tieflingButton=QPushButton("Tiefling")
+		self.tieflingButton.setToolTip("To be greeted with stares and whispers, to suffer violence and insult on the street, to see mistrust and fear in every eye.<br> This is the lot of the tiefling. <br>Their appearance and their nature are not their fault but the result of an ancient sin.")
+
+		self.humanButton=QPushButton("Human")
+		self.humanButton.setToolTip("In the reckonings of most worlds, humans are the youngest of the common races, late to arrive on the world scene and short-lived in comparison to dwarves, elves, and dragons.<br>Whatever drives them, humans are the innovators, the achievers, and the pioneers of the worlds.")
+
+		self.dragonbornButton=QPushButton("Dragonborn")
+		self.dragonbornButton.setToolTip("Born of dragons, as their name proclaims, the dragonborn walk proudly through a world that greets them with fearful incomprehension.<br> Shaped by draconic gods or the dragons themselves, dragonborn originally hatched from dragon eggs as a unique race, combining the best attributes of dragons and humanoids.")
+
+		self.halforcButton=QPushButton("Half-Orc")
+		self.halforcButton.setToolTip("Whether united under the leadership of a mighty warlock or having fought to a standstill after years of conflict, orc and human tribes sometimes form alliances, joining forces into a larger horde to the terror of civilized lands nearby. When these alliances are sealed by marriages, half-orcs are born.<br> Some half-orcs rise to become proud chiefs of orc tribes, their human blood giving them an edge over their full-blooded orc rivals.")		
+
+		self.vbox = QtWidgets.QVBoxLayout()
+		self.picbox = QtWidgets.QHBoxLayout()
+		self.buttonbox = QtWidgets.QHBoxLayout()
+
+		self.label = QLabel(self)
+		self.pixmap = QPixmap('race.jpg')
+		self.label.setPixmap(self.pixmap)
+		self.picbox.addWidget(self.label)
+
+		
+		self.buttonbox.addWidget(self.gnomeButton)		
+		self.buttonbox.addWidget(self.halflingButton)		
+		self.buttonbox.addWidget(self.dwarfButton)
+		self.buttonbox.addWidget(self.elfButton)
+		self.buttonbox.addWidget(self.halfelfButton)
+		self.buttonbox.addWidget(self.tieflingButton)
+		self.buttonbox.addWidget(self.humanButton)
+		self.buttonbox.addWidget(self.dragonbornButton)
+		self.buttonbox.addWidget(self.halforcButton)
+		
+
+		self.setLayout(self.vbox)
+		self.vbox.addLayout(self.picbox)
+		self.vbox.addLayout(self.buttonbox)
+		self.show()
+
+
+		self.resize(800, 800)
+	
+
+if __name__ == "__main__":
+	'''
+	Code from slide 15 of lecture 17
+	'''
+	app = QtWidgets.QApplication(sys.argv)
+	main_window = DND_CC_Window()
+	app.exec_()
